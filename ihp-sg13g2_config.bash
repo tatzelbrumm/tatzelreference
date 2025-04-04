094c8346778f7fa63eafb589944916dad0266509
Christoph Maier <cm.hardware.software.elsewhere@gmail.com>
2025-04-04 23:31:39 +0200

    ihp-sg13g2_config.bash : local environment variable configuration

---
+++ ihp-sg13g2_config.bash
@@ -0,0 +1,6 @@
+export PDK_ROOT=~/EDA/IHP-Open-PDK
+export PDK=ihp-sg13g2
+export PDKPATH=$PDK_ROOT/$PDK
+export STD_CELL_LIBRARY=sg13g2_stdcell
+export SPICE_USERINIT_DIR=$PDKPATH/libs.tech/ngspice
+export KLAYOUT_PATH=$PDKPATH/libs.tech/klayout:$PDKPATH/libs.tech/klayout/tech
